f4cb542ac06985e3cd778ea74577b9d97e95eeee
fm69 <info@moskopp.org>
2025-09-16 15:02:49 +0200

    V1 - Model - weitere Felder - Fix 2 - Date

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,7 +9,7 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required=True)
     description =  fields.Text()
-    date_available = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(),month=3))
+    date_available = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(),month=3))
     expected_price = fields.Float()
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer()
@@ -17,7 +17,7 @@ class EstateProperty(models.Model):
     garden_orientation =  fields.Selection([("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        seletion=[
+        selection=[
             ("new", "New"),("offer_received", "Offer received"),("offer_accepted", "Offer accepted"),("sold", "Sold"),("cancelled", "Cancelled")],
         default="new",
         copy=False,
