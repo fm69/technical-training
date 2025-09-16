80db5f1ae9445ad73f895da66ba3212dad57d8c2
fm69 <info@moskopp.org>
2025-09-16 14:13:02 +0200

    V1 - View and Menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,8 +3,9 @@
     "version": "18.0.1.0.1",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": [ "security/ir.model.access.csv"
-
+    "data": [ "security/ir.model.access.csv",
+              "views/estate_property_views.xml",
+              "views/estate_property_menues.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
