ca1e83738864ada60de039c4b4c6d75658c85dee
fm69 <info@moskopp.org>
2025-09-16 16:00:13 +0200

    V1 - List / Form View

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,9 @@
     "depends": ["base"],  # dependencies
     "data": [ "security/ir.model.access.csv",
               "views/estate_property_views.xml",
-              "views/estate_property_menus.xml"
+              "views/estate_property_menus.xml",
+              "views/estate_property_form_views.xml",
+              "views/estate_property_list_views.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
