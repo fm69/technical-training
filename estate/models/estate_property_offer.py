28bf36d6a19f36c868e3fc00840f2b7e309c7c99
fm69 <info@moskopp.org>
2025-09-17 15:10:58 +0200

    V1 - Property - add Icon / Action List Offer

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -21,3 +21,12 @@ class EstatePropertyOffer(models.Model):
     def _inverse_date_deadline(self):
         for estate in self:
             estate.validity = (estate.date_deadline - fields.Date.to_date(estate.create_date)).days
+
+    def action_accept_offer(self):
+        self.state = "accepted"
+        for offer in self:
+            offer.property_id.selling_price = offer.price
+
+    def action_refuse_offer(self):
+        self.state = "refused"
+
