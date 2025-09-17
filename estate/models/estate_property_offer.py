ba4ced565b7ae8e240bf9e6ab17bd1d266df773e
fm69 <info@moskopp.org>
2025-09-17 13:37:20 +0200

    V1 - Property - computed Field date_deadline, validity

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +8,16 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")], copy=False)
     property_id = fields.Many2one("estate_property",required=True)
     partner_id = fields.Many2one("res.partner",required=True)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    validity = fields.Integer(default=7)
+
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for estate in self:
+            create_date = estate.create_date or fields.Date.today()
+            estate.date_deadline = fields.Date.add(create_date, days=estate.validity)
+
+    def _compute_date_deadline(self):
+        for estate in self:
+            estate.validity = (estate.date_deadline - fields.Date.to_date(estate.create_date)).days
